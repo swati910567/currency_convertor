e608c1a6a7684d5bc4e924cf590718f804c73ae4
Swati Rani <124399483+swati910567@users.noreply.github.com>
2024-01-14 00:52:34 +0530

    Update Input.jsx

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ function Input({
     onAmountChange,
     onCurrencyChange,
     currencyOptions=[],
-    selectCurrency="usd",
+    selectCurrency="",
     amountDisable=false,
     currencyDisable=false,
     
